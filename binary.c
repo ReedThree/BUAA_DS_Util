2dd6f58e6e42d8bd33654e5de9d861bad25a5bc5
ReedThree <saite@buaa.edu.cn>
2024-06-22 20:02:08 +0800

    binary.c: Fix logic bug in lessThan and greaterThan.

--- binary.c
+++ binary.c
@@ -1,80 +1,52 @@
- 
-int find(int arr[], int n, int target)
-{
+
+int find(int arr[], int n, int target) {
     int hi = n - 1;
     int lo = 0;
     int mid = (hi + lo) / 2;
-    while (lo <= hi)
-    {
+    while (lo <= hi) {
         mid = (hi + lo) / 2;
-        if (arr[mid] < target)
-        {
+        if (arr[mid] < target) {
             lo = mid + 1;
-        }
-        else if (arr[mid] > target)
-        {
+        } else if (arr[mid] > target) {
             hi = mid - 1;
-        }
-        else
-        {
+        } else {
             return mid;
         }
     }
     return -1;
 }
 
-int equalOrGreaterThan(int arr[], int n, int target)
-{
+int greaterThan(int arr[], int n, int target) {
     int hi = n - 1;
     int lo = 0;
     int mid = (hi + lo) / 2;
-    while (lo <= hi)
-    {
+    while (lo <= hi) {
         mid = (hi + lo) / 2;
-        if (arr[mid] < target)
-        {
+        if (arr[mid] < target) {
+            lo = mid + 1;
+        } else if (arr[mid] > target) {
+            hi = mid - 1;
+        } else {
             lo = mid + 1;
         }
-        else if (arr[mid] > target)
-        {
-            hi = mid - 1;
-        }
-        else
-        {
-            hi = mid - 1;
-        }
-    }
-    if (lo < 0)
-    {
-        return 0;
     }
     return lo;
 }
 
-int equalOrLessThan(int arr[], int n, int target)
-{
+int lessThan(int arr[], int n, int target) {
     int hi = n - 1;
     int lo = 0;
     int mid = (hi + lo) / 2;
-    while (lo <= hi)
-    {
+    while (lo <= hi) {
         mid = (hi + lo) / 2;
-        if (arr[mid] < target)
-        {
+        if (arr[mid] < target) {
             lo = mid + 1;
-        }
-        else if (arr[mid] > target)
-        {
+        } else if (arr[mid] > target) {
+            hi = mid - 1;
+        } else {
             hi = mid - 1;
         }
-        else
-        {
-            lo = mid + 1;
-        }
     }
-    if (hi < 0)
-    {
-        return 0;
-    }
+
     return hi;
 }
